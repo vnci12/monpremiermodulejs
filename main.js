0780f6b7ef9faf973ae22f4109c22901a3e90cca
vnci12 <mohayowane@gmail.com>
2026-01-20 14:16:40 +0300

    ajout d'un nouveau ficher user

--- main.js
+++ main.js
@@ -8,3 +8,10 @@ import monHeure from "./generateHeure.js";
 console.log("Main - mon heure est : " + monHeure.monHeure() + "h" + "min" + "s");
 
 document.querySelector("#heure").textContent = monHeure.monHeure() + "h" + "min" + "s";
+
+// j'importe l'objet user
+import user from "./user.js";
+
+console.log("Main - utilisateur : ", user);
+
+document.querySelector("#user").textContent = "Utilisateur : " + user.user.prenom + " " + user.user.nom + ", Email : " + user.user.email + ", Mot de passe : " + user.user.motDePasse;
---
+++ user.js
@@ -0,0 +1,12 @@
+// Je crée un objet user
+const user = {
+    nom: "Modex",
+    prenom: "DWWM",
+    email: "modex.dwwm@example.com",
+    motDePasse: "securePassword123"
+};
+
+// J'exporte l'objet user pour qu'il soit accessible depuis un autre fichier
+export default {
+    user
+};
